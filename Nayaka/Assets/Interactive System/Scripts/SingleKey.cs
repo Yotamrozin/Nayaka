3a24cbb1228da38f6143f472c7f4f6f2fc495ef6
Yotam Rozin <yotam.rozin@gmail.com>
2019-03-08 16:14:57 +0100

    Added Resting Time and fixed Key Holding
    Fixed - Key holding was not working because of duration
    
    Added - Resting time check when key is clicked - making it inhibiting instead of exciting. Still needs to have key holding be effected by resting time.

--- Nayaka/Assets/Interactive System/Scripts/SingleKey.cs
+++ Nayaka/Assets/Interactive System/Scripts/SingleKey.cs
@@ -13,6 +13,7 @@ public class SingleKey : MonoBehaviour
     //INHERITED FROM BODY
 
     //excitement (0<x<1) or inhibition (-1<x<0).
+    public float BaseForce;
     public float Excitement;
     public float DecayTime;
 
@@ -20,11 +21,13 @@ public class SingleKey : MonoBehaviour
     //the desired stimulation
     //will be converted to duration/number of clicks the key wishes to be pressed 
     //after which it goes from excitement to inhibition
+    //should i make key holding, number of clicks, and resting time codependent?
     [Range(0f, 1f)]
     public float DesiredStimulation;
     public float DesiredTime;
     public int DesiredClicks;
     public float DesiredRest;
+    private float RestViolationFactor;
 
     //how many keys to each direction does the pressing of the key affect
     public float areaOfEffect;
@@ -33,12 +36,14 @@ public class SingleKey : MonoBehaviour
 
     //BACK PROPAGATION
     public float force = 0;
-    public float forceIncrement = 0.1f;
+    public float ExcitementClickIncrement = 0.1f;
+    public float KeyHoldExcitementFactor = 0.01f;
 
     private float OriginalExcitement;
     private bool isPressed; //used for duration of press
-    private float duration, durationFactor = 0.01f;
+    private float duration;
     private int numberOfClicks;
+    private float LastPress;
 
     // Use this for initialization
     void Start()
@@ -55,85 +60,96 @@ public class SingleKey : MonoBehaviour
 
     //Lerp Over Time
     private float TimeLerpStarted;
-    public float ExcitementDecayTime;
-    public float ClicksDecayTime;
+   // public float ExcitementDecayTime;
+   // public float ClicksDecayTime;
 
     // Update is called once per frame
     void Update()
     {
-        DecayExcitementAndNumberOfClicksToOriginalValue();
-
+        //Click + Key Hold (calculates excitement) -> Rest
         CheckIfKeyIsPressedSendForceAndAddClickCount();
-
-        KeyHoldingExciteOrInhibitAndCountDuration();
-
-        SendForce();
-
-        CheckIfKeyUpAndReset();
-
-    }
-
-    public void Reset()
-    {
-        Excitement = OriginalExcitement;
-        numberOfClicks = 0;
-    }
-
-    public void SendForce()
-    {
-        transform.position += new Vector3(0, Excitement * force, 0);
-        print(name + Excitement);
-    }
-
-    //DECAY (smoothed)
-    public void DecayExcitementAndNumberOfClicksToOriginalValue()
-    {
-        if (Excitement != OriginalExcitement)
+        if (isPressed)
         {
-            Excitement = SmoothStep(Excitement, OriginalExcitement, DecayTime);
+            KeyHolding();
+
+            SendForce();
+
+            CheckIfKeyUpAndReset();
         }
-        if (numberOfClicks != 0)
+        if (!isPressed)
         {
-            numberOfClicks = (int)SmoothStep(numberOfClicks, 0, DecayTime);
+            DecayExcitementToOriginalValue();
+            DecayNumberOfClicksToOriginalValue();
         }
+
     }
 
 
+    
     //ON KEY DOWN
     private void CheckIfKeyIsPressedSendForceAndAddClickCount()
     {
         if (Input.GetKeyDown(thisKeyCode))
         {
+            print(thisKeyCode + "clicked");
+            //check if the key rested enough
+            if (Time.time - LastPress < DesiredRest)
+            { 
+                RestViolationFactor = Mathf.Lerp(1, 0, Time.time - LastPress);
+                print(RestViolationFactor + "Rest Violation Factor");
+                Excitement -= ExcitementClickIncrement*RestViolationFactor;
+            }
+            else
+            {
+                Excitement += ExcitementClickIncrement;
+            } 
             numberOfClicks += 1;
             isPressed = true;
+            LastPress = Time.time;
             duration = 0;
-            force += forceIncrement;
         }
     }
 
 
     //KEY HOLD
-    //calculate duration and increase or decrease excitement 
+    //calculate duration and increase or inhibition 
     //according to desired time
-    private void KeyHoldingExciteOrInhibitAndCountDuration()
+    private void KeyHolding()
     {
-
         if (isPressed)
         {
-            duration += Time.deltaTime;
-
-            if (duration < DesiredTime || numberOfClicks < DesiredClicks)
+            duration = Time.time - LastPress;
+            if (RestViolationFactor > 0)
             {
-
-                if (Excitement < 1)
-                    Excitement = Excitement + (duration * durationFactor);
+                if (duration < DesiredTime)
+                {
+                    print(thisKeyCode + "is held");
+                    if (Excitement < 1)
+                    {
+                        Excitement += KeyHoldExcitementFactor;
+                        print("excitement up from holding " + duration);
+                    }
+                    else
+                    {
+                        Debug.Log("reached maximum");
+                    }
+                }
             }
-            if (duration > DesiredTime || numberOfClicks > DesiredClicks)
-            {
-
-                if (Excitement > -1)
-                    Excitement = Excitement - (duration * durationFactor);
-            }
+            if (RestViolationFactor <= 0)
+                {
+                    if (duration > DesiredTime)
+                    {
+                        if (Excitement > -1)
+                        {
+                            Excitement -= KeyHoldExcitementFactor;
+                            print("excitement down from holding " + duration);
+                        }
+                        else
+                        {
+                            Debug.Log("reached minimum");
+                        }
+                    }
+                }
         }
     }
 
@@ -144,7 +160,23 @@ public class SingleKey : MonoBehaviour
         {
             isPressed = false;
             TimeLerpStarted = Time.time;
-            durationFactor = 0;
+            KeyHoldExcitementFactor = 0;
+
+        }
+    }
+    //DECAY (smoothed)
+    public void DecayExcitementToOriginalValue()
+    {
+        if (Excitement != OriginalExcitement)
+        {
+            Excitement = SmoothStep(Excitement, OriginalExcitement, DecayTime);
+        }
+    }
+    public void DecayNumberOfClicksToOriginalValue()
+    {
+        if (numberOfClicks != 0)
+        {
+            numberOfClicks = (int)SmoothStep(numberOfClicks, 0, DecayTime);
         }
     }
 
@@ -155,5 +187,18 @@ public class SingleKey : MonoBehaviour
         float PercentageComplete = TimeSinceStarted / LerpTime;
         var result = Mathf.SmoothStep(LerpStartValue, LerpEndValue, PercentageComplete);
         return result;
+    }
+
+    public void Reset()
+    {
+        Excitement = OriginalExcitement;
+        numberOfClicks = 0;
+    }
+
+    public void SendForce()
+    {
+        print(Excitement + " " + thisKeyCode);
+        //       transform.position += new Vector3(0, Excitement * force, 0);
+        //        print(name + Excitement);
     }
 }
